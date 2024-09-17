1c0d1bc7a8a79479c2d400fb87efe9d6303d7d5d
reimo <reimo.nurges@tktk.ee>
2024-09-17 17:09:30 +0300

    create new app

---
+++ FirstApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("nimi:");
+string username;
+username = Console.ReadLine();
+Console.WriteLine("su nimi on " + username);
+Console.WriteLine($"su nimi on {username}");
